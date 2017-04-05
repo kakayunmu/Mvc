3bf50cd880155c246820b78b9bc5e0dc063e1967
Steve Sanderson <stevesa@microsoft.com>
2017-04-05 14:12:31 +0100

    CR feedback: Regenerate resx files

--- src/Microsoft.AspNetCore.Mvc.Core/Properties/Resources.Designer.cs
+++ src/Microsoft.AspNetCore.Mvc.Core/Properties/Resources.Designer.cs
@@ -813,16 +813,14 @@ namespace Microsoft.AspNetCore.Mvc.Core
         /// </summary>
         internal static string ModelBinding_MissingRequestBodyRequiredMember
         {
-            get { return GetString("ModelBinding_MissingRequestBodyRequiredMember"); }
+            get => GetString("ModelBinding_MissingRequestBodyRequiredMember");
         }
 
         /// <summary>
         /// A non-empty request body is required.
         /// </summary>
         internal static string FormatModelBinding_MissingRequestBodyRequiredMember()
-        {
-            return GetString("ModelBinding_MissingRequestBodyRequiredMember");
-        }
+            => GetString("ModelBinding_MissingRequestBodyRequiredMember");
 
         /// <summary>
         /// The parameter conversion from type '{0}' to type '{1}' failed because no type converter can convert between these types.
